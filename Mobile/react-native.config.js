aa0d23dcb29e839cfae51b6b2dbcc00ab180d1da
Okan SARICA <okansarica@gmail.com>
2026-03-01 15:03:51 +0000

    upgrade

--- Mobile/react-native.config.js
+++ Mobile/react-native.config.js
@@ -1,18 +1,1 @@
-module.exports = {
-  commands: [],
-  platforms: {
-    ios: {
-      projectDirectory: './ios',
-    },
-  },
-  project: {
-    ios: {
-      sourceDir: './ios',
-      xcodeProject: {
-        name: 'Spendly.xcworkspace',
-        isWorkspace: true,
-      },
-    },
-  },
-};
-
+module.exports = {};
--- Mobile/src/screens/auth/LoginScreen.tsx
+++ Mobile/src/screens/auth/LoginScreen.tsx
@@ -30,7 +30,7 @@ export default function LoginScreen() {
   const isValid = email.length > 0 && password.length > 0;
 
   useEffect(() => {
-    GoogleSignin.configure();
+    //TODO GoogleSignin.configure();
   }, []);
 
   const handleLogin = async () => {
